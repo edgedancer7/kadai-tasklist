1ab139401c029bd4177562ac7e5315236b01800a
keitarousugiyama <ks@kourakusha.com>
2017-07-03 12:40:08 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasklist#index'
-  resources :tasklist
+  root to: 'tasklists#index'
+  resources :tasklists
 end
